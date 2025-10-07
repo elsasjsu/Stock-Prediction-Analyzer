31789ef36bbfbf7cb30466cbc020143e9e7bad43
Elsa Rose <elsa.rose@sjsu.edu>
2025-10-07 14:00:52 -0700

    LAB1_SROCK_PRICE_FORECAST_PIPELINE.py

--- lab1_STOCK_ETL.py
+++ lab1_STOCK_ETL.py
@@ -1,4 +1,3 @@
-
 from airflow import DAG
 from airflow.decorators import task
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
@@ -10,87 +9,9 @@ def return_snowflake_conn():
     hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
     return hook.get_conn()
 
-# Function to download stock data
-@task
-def download_stock_data(tickers, period="180d"):
-  data = yf.download(tickers, period=period)
-  return data
 
 
-# Function to create table
-def create_table(table):
-    conn = return_snowflake_conn()
-    try:
-        cur = conn.cursor()
-        try:
-            cur.execute(
-                f"""
-                CREATE OR REPLACE TABLE {table} 
-                (symbol VARCHAR, open FLOAT, high FLOAT, low FLOAT, close FLOAT, 
-                volume INT, date DATE, PRIMARY KEY (symbol, date)
-                )"""
-            )
-        finally:
-            cur.close()
-        conn.commit() 
-    except Exception as e:
-        conn.rollback()
-        raise
-    finally:
-        conn.close()
-
-
-# Function to insert data into the table
-@task
-def load_records(table, results):
-    try:
-        conn = return_snowflake_conn()
-        create_table(table)
-        cursor = conn.cursor()
-        try:
-            symbols = results.columns.get_level_values('Ticker').unique()
-            for date, row in results.iterrows():
-                for symbol in symbols:
-                    open_price = row["Open"][symbol]
-                    high = row["High"][symbol]
-                    low = row["Low"][symbol]
-                    close = row["Close"][symbol]
-                    volume = row["Volume"][symbol]
-                    date_str = date.strftime('%Y-%m-%d')
-
-
-                    merge_sql = f"""
-                    MERGE INTO {table} t
-                    USING (SELECT %s AS symbol,
-                                %s AS open,
-                                %s AS high,
-                                %s AS low,
-                                %s AS close,
-                                %s AS volume,
-                                %s AS date) s
-                    ON t.symbol = s.symbol AND t.date = s.date
-                    WHEN MATCHED THEN UPDATE SET
-                        t.open = s.open,
-                        t.high = s.high,
-                        t.low = s.low,
-                        t.close = s.close,
-                        t.volume = s.volume
-                    WHEN NOT MATCHED THEN INSERT (symbol, open, high, low, close, volume, date)
-                    VALUES (s.symbol, s.open, s.high, s.low, s.close, s.volume, s.date)
-                    """
-                    cursor.execute(merge_sql, (symbol, open_price, high, low, close, volume, date_str))
-        finally:
-            cursor.close()
-        conn.commit()
-    except Exception:
-        conn.rollback()
-        raise
-    finally:
-        conn.close()
-
-
-
-"""## Function to create ML forecast in snowflake to predict the next 7 days of the stock price"""
+# Function to create ML forecast in snowflake to predict the next 7 days of the stock price
 @task
 def train(train_input_table, train_view, forecast_function_name):
     conn = return_snowflake_conn()
@@ -149,11 +70,11 @@ def predict(forecast_function_name, train_input_table, forecast_table, final_tab
 
 
 with DAG(
-    dag_id='stocks_prediction_analysis',
-    start_date=datetime(2025, 10, 5),
+    dag_id='stock_price_forecast_pipeline',
+    start_date=datetime(2025, 10, 6),
     catchup=False,
-    tags=['TRAIN', 'PREDICT'],
-    schedule_interval='30 2 * * *', 
+    tags=['ML'],
+    schedule_interval='30 3 * * *', 
 ) as dag:
 
     train_input_table = "raw.stock_prices"
@@ -163,10 +84,7 @@ with DAG(
     final_table = "analytics.final_result"
     tickers = ["NVDA","TSLA"]
     
-    records = download_stock_data(tickers, period="180d")
-    load_data = load_records(train_input_table, records)
     train_task = train(train_input_table, train_view, forecast_function_name)
     predict_task = predict(forecast_function_name, train_input_table, forecast_table, final_table)
 
-    records >> load_data >> train_task >> predict_task
-
+    train_task >> predict_task
